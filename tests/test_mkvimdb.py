fd35b9de4af11cb029db044d1b0130777dd1596b
dimitry <dimitry.dukhovny@gmail.com>
2020-09-24 08:37:13 -0700

    So, may I can test a little.

--- tests/test_mkvimdb.py
+++ tests/test_mkvimdb.py
@@ -12,10 +12,13 @@ class TestMkvimdb(unittest.TestCase):
     """Tests for `mkvimdb` package."""
 
     def setUp(self):
-        """Set up test fixtures, if any."""
+        """Give us an IMDB browser object"""
+        self.m = mkvimdb.moviecursor()
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        pass
 
-    def test_000_something(self):
-        """Test something."""
+    def test_default_query(self):
+        """We should see the right canonical name for Pulp Fiction (1994)."""
+        self.assertEqual(self.m.tagdata['filename'], 'tarantino,quentin-pulp_fiction-1994', 'Misidentified film!')
